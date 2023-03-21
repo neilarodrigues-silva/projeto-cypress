d72effabad9db89ccba6e0f8eb958a968fd277f5
Neila Rodrigues <naal@gft.com>
2023-03-21 10:41:56 -0300

    Conf cypress

---
+++ cypress/integration/1-getting-started/alura-busca.spec.js
@@ -0,0 +1,13 @@
+describe('alura busca cursos', () => {
+
+    beforeEach(() => {
+        cy.visit("https://www.alura.com.br");
+    })
+
+    it('buscar curso de java', () => {
+        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('.header-barraBusca-form-submit').click();
+        cy.get(':nth-child(6) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
+        .should('have.text', 'Formação Java e Spring Boot');
+    })
+})
